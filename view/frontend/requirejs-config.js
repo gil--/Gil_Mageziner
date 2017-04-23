7d064d80f656554620bc725005987affa924fda3
Gil Greenberg <ggreenberg@somethingdigital.com>
2017-04-23 13:24:26 -0400

    Load Google Webfont loader through requirejs and avoid anonymous define

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,7 @@ var config = {
 		'*': {
 			'mageziner/peekaboo': 'Gil_Mageziner/js/peekaboo-toggle.jquery',
 			'mageziner/select2': 'Gil_Mageziner/js/select2.min',
+			'webfont': '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js',
 		},
 	},
 	shim: {
